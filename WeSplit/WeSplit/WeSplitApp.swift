1e4159d45b6cc5857302e3e3d324fec3328a7374
Musa Rafik <musarafik@Musas-MacBook-Pro.local>
2022-06-05 19:43:58 -0400

    did base requirements for WeSplit

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Musa Rafik on 6/3/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
